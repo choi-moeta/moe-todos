051232b5bf516e90fe3822c80e720dd7193c6ddd
moeta <teqqmu@gmail.com>
2021-08-21 21:49:19 +0700

    feat: create header component

---
+++ src/components/moe-header.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { isDark, toggleDark } from '$/logic'
+</script>
+
+<template>
+  <nav
+    class="
+      py-4
+      flex items-center gap-4
+    "
+  >
+    <moe-profile class="ml-auto" />
+
+    <button class="flex hover:text-orange-500" title="toggle theme" @click="toggleDark">
+      <carbon-moon v-if="isDark" />
+      <carbon-sun v-else />
+    </button>
+
+    <a
+      class="flex hover:text-orange-500"
+      rel="noreferrer"
+      href="https://github.com/antfu/vitesse-lite"
+      target="_blank"
+      title="source on github"
+    >
+      <carbon-logo-github />
+    </a>
+  </nav>
+</template>
