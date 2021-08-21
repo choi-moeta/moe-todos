33609ecb14d762f3b6538238b816c6a142ee66c7
moeta <teqqmu@gmail.com>
2021-08-21 21:48:16 +0700

    feat: create button

---
+++ src/components/moe-button.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { PropType } from 'vue'
+
+defineProps({
+  type: {
+    type: String as PropType<'primary' | 'ghost'>,
+    default: 'primary',
+  },
+})
+</script>
+
+<template>
+  <button
+    class="
+      px-4 py-1.5
+      lowercase
+      rounded
+    "
+    :class="{
+      'bg-orange-500 text-white hover:(bg-orange-400)': type == 'primary',
+      'hover:bg-gray-100': type == 'ghost',
+    }"
+  >
+    <slot />
+  </button>
+</template>
