eed28f5d0022c87bfd22dac0f30c2d55fa911af8
moeta <teqqmu@gmail.com>
2021-08-21 21:51:08 +0700

    feat: create todo-create component

---
+++ src/components/moe-todo-create.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+const emit = defineEmits<{
+  (event: 'create', text: string): void
+}>()
+
+const textEl = ref<HTMLDivElement | null>()
+
+function onCreate() {
+  const text = textEl.value?.innerText
+  if (text && textEl.value) {
+    textEl.value.innerText = ''
+    emit('create', text)
+  }
+}
+</script>
+
+<template>
+  <div>
+    <div
+      ref="textEl"
+      contenteditable
+      class="
+        border border-gray-300
+        px-4 py-2
+        rounded
+      "
+      placeholder="create todo"
+      type="text"
+      name=""
+    >
+    </div>
+    <div class="flex gap-2 mt-4">
+      <moe-button @click="onCreate">
+        Create
+      </moe-button>
+      <moe-button type="ghost">
+        Cancel
+      </moe-button>
+    </div>
+  </div>
+</template>
