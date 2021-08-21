4c358c5fdda0f3abff901a694b32e186451d2f45
moeta <teqqmu@gmail.com>
2021-08-21 21:51:39 +0700

    feat: implement todos page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,13 +1,18 @@
 <script setup lang="ts">
-import { useRouter } from 'vue-router'
-import { isLoggedIn } from '$/api'
-
-const router = useRouter()
-if (!isLoggedIn())
-  router.push('/auth')
 </script>
 
 <template>
-  <div>
+  <div class="w-full flex justify-center">
+    <div class="w-full max-w-screen-sm">
+      <moe-title>
+        задачи
+      </moe-title>
+      <moe-todos class="w-full text-lg mt-4" />
+    </div>
   </div>
 </template>
+
+<route lang="yaml">
+meta:
+  onlyUser: true
+</route>
