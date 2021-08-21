adf17b18887c2067bfc016a33e4719cfefff789e
moeta <teqqmu@gmail.com>
2021-08-21 21:48:55 +0700

    feat: create title component

---
+++ src/components/moe-title.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { PropType } from 'vue'
+
+const props = defineProps({
+  level: {
+    type: Number as PropType<1 | 2 | 3 | 4 | 5 | 6>,
+    default: 1,
+  },
+})
+
+const el = computed(() => `h${props.level}`)
+</script>
+
+<template>
+  <component :is="el" class="my-0 text-black dark:text-white">
+    <slot></slot>
+  </component>
+</template>
