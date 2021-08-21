e0862a86c2ffa10a0ccd119e531a58a39e48413f
moeta <teqqmu@gmail.com>
2021-08-21 21:50:05 +0700

    feat: update 404 page

--- src/pages/[...all].vue
+++ src/pages/[...all].vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
-    Not Found
+  <div class="mt-40">
+    <moe-title>
+      страница не найдена
+    </moe-title>
+    <moe-text class="mt-2">
+      Вернуться на <moe-link href="/">
+        главную
+      </moe-link>
+    </moe-text>
   </div>
 </template>
 
